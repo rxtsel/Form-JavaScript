aab7217ad8416138c654b23be2ca0c5293d83397
Rxtsel <cristhixnn@hotmail.com>
2022-01-14 15:57:20 -0500

    v.0.1

--- js/form.js
+++ js/form.js
@@ -101,7 +101,10 @@ formulario.addEventListener('submit', (e) => {
             icono.classList.remove('formulario__grupo--correcto')
         });
     } else {
-        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo')
+        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+        setTimeout(() => {
+            document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+        }, 4000);
     };
 });
 
